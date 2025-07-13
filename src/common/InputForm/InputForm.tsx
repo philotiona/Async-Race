e04d323a47457726b65740cd33fff1bdede0e6a7
truthofgod <123151544+truthofgod@users.noreply.github.com>
2025-07-14 01:25:58 +0500

    feat: Add CRUD logic

--- src/common/InputForm/InputForm.tsx
+++ src/common/InputForm/InputForm.tsx
@@ -2,7 +2,7 @@ import { useState, type FormEvent, type ReactNode } from "react";
 import styles from "./InputForm.module.css"
 import Button from "../Button/Button";
 import { useDispatch, useSelector } from "react-redux";
-import { addCar } from "../../store/AddCarSlice";
+import { addCar, updateCar } from "../../store/AddCarSlice";
 import type { RootState } from "../../store/store";
 interface PropTypes {
     textButton: string
@@ -11,20 +11,27 @@ interface PropTypes {
 export default function InputForm({textButton}: PropTypes): ReactNode {
     const dispatch = useDispatch();
     const [car, setCar] = useState<string>("")
-    const [color, setColor] = useState<string>("")
+    const [newColor, setColor] = useState<string>("")
     const carSelector = useSelector((state: RootState) => state.addCar.cars)
     const maxId = Math.max(...carSelector.map(item => item.id),0) + 1
+
+    const carId = useSelector((state: RootState) => state.addCar.selectedCarId)
+
     const handleSubmitCreate = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         dispatch(addCar({
             name: car,
-            color: color,
+            color: newColor,
             id: maxId
         }))
         
     }
-    const handleSubmitUpdate =(e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();}
+    const handleSubmitUpdate = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if(carId) {
+            dispatch(updateCar({id: carId, color: newColor, name: car}))
+        }
+    }
     return(
         <div className={styles.wrapper}>
             <form className={styles.wrapper} action="submit" onSubmit={textButton === "Create" ? handleSubmitCreate : handleSubmitUpdate}>
